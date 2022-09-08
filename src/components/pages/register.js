30c22057b25065dc54fd33c94f6e55432885c368
zrogba <zrogba@gmail.com>
2022-09-09 01:10:50 +0300

    halfway

--- src/components/pages/register.js
+++ src/components/pages/register.js
@@ -2,25 +2,29 @@ import React from "react";
 import './register.css';
 
 const Register = () => {
-
+	const handleInput = () => {
+		
+	}
 	return (
+		<>
 		<div className="container">
+			
 <form>
   <div class="form-group">
     <label for="exampleInputEmail1">First Name</label>
-    <input type="text" class="form-control" id="InputEmail1"   placeholder="First Name" />
+    <input type="text" class="form-control" id="InputEmail1"   placeholder="First Name" value="test" onChange={handleInput}/>
 	</div>
 	<div class="form-group">
 		<label for="exampleInputEmail1">Last Name</label>
-    <input type="text" class="form-control" id="exampleInputEmail1"  placeholder="Last Name" />
+    <input type="text" class="form-control" id="exampleInputEmail1"  placeholder="Last Name" value="test" onChange={handleInput} />
 	</div>
 	<div class="form-group">
 		<label for="exampleInputEmail1">Phone Number</label>
-    <input type="text" class="form-control" id="exampleInputEmail1"  placeholder="Phone" />
+    <input type="text" class="form-control" id="exampleInputEmail1"  placeholder="Phone" value="test" onChange={handleInput}/>
 	</div>
 	<div class="form-group">
 		<label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value="test"/>
 	</div>
 	
   <div class="form-group row">
@@ -33,7 +37,7 @@ const Register = () => {
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </div>
-
+</>
 )
 
 }
